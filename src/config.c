6bc2bdf3adb6d22f998336bcc8c4a6a11e640cec
Jesper Kristiansson <jeppe.00@live.se>
2022-08-15 14:43:10 +0200

    handled case where config file doesn't exist

--- src/config.c
+++ src/config.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define CONFIG_PATH_FROM_HOME "/.shell_config"
 
@@ -12,6 +13,10 @@ void run_config(){                  //check if config file exists
     char path[MAXBUF];
     strncpy(path, getenv("HOME"), MAXBUF-strlen(CONFIG_PATH_FROM_HOME));
     strcat(path, CONFIG_PATH_FROM_HOME);
+
+    if(access(path, R_OK) != 0){
+        return;
+    }
     FILE *config_file = fopen(path, "r");
 
     char buf[MAXBUF];
